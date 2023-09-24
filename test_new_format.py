69fc45aa87a6c9e879dac708535197b8fee17313
MyFrei <myfrei97@gmail.com>
2023-09-24 18:07:56 +0300

    new format 1

--- test_new_format.py
+++ test_new_format.py
@@ -20,8 +20,15 @@ class StartFrame(ttk.Frame):
 
         self.use_custom_dir = tk.BooleanVar(value=False)
 
-        self.switch = Switch(self)
-        self.switch.pack(anchor=tk.W, pady=5)
+        self.switch_frame = ttk.Frame(self)  # Дополнительный фрейм для свитча и метки
+        self.switch_frame.pack(anchor=tk.W, pady=5)
+
+        self.switch = Switch(self.switch_frame)  # Изменен контейнер на self.switch_frame
+        self.switch.pack(side="left")
+
+        self.switch_label = ttk.Label(self.switch_frame, text="Выбрать другую директорию")
+        self.switch_label.pack(side="left", padx=5)
+
         self.switch.bind('<<SwitchToggled>>', lambda e: self.toggle_dir_choice())
 
         self.dir_entry = ttk.Entry(self, state='disabled')
@@ -80,8 +87,8 @@ class XMLApp:
         self.root = root
         available_themes = self.root.get_themes()  # Получить доступные темы
         print("Available themes: ", available_themes)  # Вывести список доступных тем
-        if 'breeze' in available_themes:
-            self.root.set_theme("breeze")  # Применение темы , если она доступна
+        if 'scidsand' in available_themes:
+            self.root.set_theme("scidsand")  # Применение темы , если она доступна
         else:
             print("Theme 'azure' is not available. Please select another theme.")
 
@@ -98,7 +105,7 @@ class XMLApp:
         self.menu = tk.Menu(self.root)
         self.root.config(menu=self.menu)
         self.file_menu = tk.Menu(self.menu)
-        self.menu.add_cascade(label="File", menu=self.file_menu)
+        #self.menu.add_cascade(label="File", menu=self.file_menu)
         self.file_menu.add_command(label="Open", command=self.load_xml)
 
     def start_analysis(self, file_path):
@@ -160,31 +167,32 @@ class XMLApp:
             self.populate_tree(tree, child, id, level + 1)
 
 
-class Switch(ttk.Frame):
+class Switch(tk.Canvas):
     def __init__(self, parent, *args, **kwargs):
-        super().__init__(parent, *args, **kwargs)
+        super().__init__(parent, width=40, height=20, *args, **kwargs)
+        self.value = False  # Переключатель выключен по умолчанию
+        self.configure(borderwidth=0, highlightthickness=0)  # Убираем рамку
 
-        self._variable = tk.BooleanVar(value=False)
+        # Сам переключатель
+        self.rect = self.create_rectangle(1, 1, 40, 20, outline='black', fill='red', width=1, tags='rect')
 
-        self._off_button = ttk.Button(self, text='Off', command=self._toggle)
-        self._off_button.pack(side='left', fill='x', expand=True)
+        # Белый кругляш
+        self.oval = self.create_oval(2, 2, 18, 18, outline='black', fill='white', tags='oval')
 
-        self._on_button = ttk.Button(self, text='On', command=self._toggle)
-        self._on_button.pack(side='left', fill='x', expand=True)
+        self.tag_bind('oval', '<1>', self.toggle)
+        self.tag_bind('rect', '<1>', self.toggle)
 
-    def _toggle(self):
-        self._variable.set(not self._variable.get())
-        self._trigger_event()
-
-    def _trigger_event(self):
-        self.event_generate('<<SwitchToggled>>')
+    def toggle(self, event=None):
+        self.value = not self.value
+        fill_color = 'green' if self.value else 'red'
+        oval_x0, oval_y0, oval_x1, oval_y1 = (22, 2, 38, 18) if self.value else (2, 2, 18, 18)
+        self.coords(self.oval, oval_x0, oval_y0, oval_x1, oval_y1)
+        self.itemconfig(self.rect, fill=fill_color)
+        self.event_generate('<<SwitchToggled>>')  # Генерируем событие при переключении
 
     @property
     def is_on(self):
-        return self._variable.get()
-
-    def set_state(self, state):
-        self._variable.set(state)
+        return self.value
 
 
 def main():
