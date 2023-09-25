ed072ef532138fa93ac79e6c6f8ce790e99419e8
MyFrei <myfrei97@gmail.com>
2023-09-26 02:30:07 +0300

    new format 3

--- test_new_format.py
+++ test_new_format.py
@@ -1,57 +1,59 @@
 import os
 import tkinter as tk
-from tkinter import ttk
+from tkinter import ttk, YES, BOTH
 from tkinter import filedialog
 import xml.etree.ElementTree as ET
 from PIL import Image, ImageTk
 from ttkthemes import ThemedTk
-from tkinter import YES, BOTH
-
+from tkinter import messagebox
 
 class StartFrame(ttk.Frame):
     def __init__(self, container, controller, *args, **kwargs):
         super().__init__(container, *args, **kwargs)
         self.controller = controller
 
-        # Добавление логотипа
-        img_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'photo.jpg')
-        img = Image.open(img_path)  # Загрузка изображения с помощью Pillow
-        img = img.resize((300, 300))  # установка размера изображения
-        self.logo = ImageTk.PhotoImage(img)
-        ttk.Label(self, image=self.logo).pack(side="right", padx=5)
+        # Левая часть
+        self.switch_frame = ttk.Frame(self)
+        self.switch_frame.grid(row=0, column=0, padx=5, pady=5, sticky='nw')
 
-        self.use_custom_dir = tk.BooleanVar(value=False)
-
-        self.switch_frame = ttk.Frame(self)  # Дополнительный фрейм для свитча и метки
-        self.switch_frame.pack(anchor=tk.W, pady=5)
-
-        self.switch = Switch(self.switch_frame)  # Изменен контейнер на self.switch_frame
-        self.switch.pack(side="left")
+        self.switch = Switch(self.switch_frame)
+        self.switch.grid(row=0, column=0, padx=5, pady=5)
 
         self.switch_label = ttk.Label(self.switch_frame, text="Выбрать другую директорию")
-        self.switch_label.pack(side="left", padx=5)
-
+        self.switch_label.grid(row=0, column=1, padx=5, pady=5)
         self.switch.bind('<<SwitchToggled>>', lambda e: self.toggle_dir_choice())
 
         self.dir_entry = ttk.Entry(self, state='disabled')
-        self.dir_entry.pack(fill=tk.X, padx=5, pady=5)
+        self.dir_entry.grid(row=1, column=0, padx=5, pady=5, sticky='ew')
+
         self.dir_button = ttk.Button(self, text="Выбрать папку", command=self.choose_directory, state='disabled')
-        self.dir_button.pack(pady=5)
+        self.dir_button.grid(row=2, column=0, padx=5, pady=5)
 
-        self.file_listbox = tk.Listbox(self, width=25)  # Установка ширины выбра файла
-        self.file_listbox.pack(fill=tk.BOTH, expand=True)
+        self.file_listbox = tk.Listbox(self, width=25)
+        self.file_listbox.grid(row=3, column=0, padx=5, pady=5, sticky='nsew')
 
-        ttk.Button(self, text="Start", command=self.start).pack(pady=5)
+        ttk.Button(self, text="Start", command=self.start).grid(row=4, column=0, padx=5, pady=5)
+
+        # Правая часть
+        img_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'photo.jpg')
+        img = Image.open(img_path)
+        img = img.resize((300, 300))
+        self.logo = ImageTk.PhotoImage(img)
+        ttk.Label(self, image=self.logo).grid(row=0, column=1, rowspan=4, padx=5, pady=5, sticky='ne')
 
         self.theme_combobox = ttk.Combobox(self, values=self.controller.available_themes)
         if self.controller.available_themes:
-            self.theme_combobox.set(
-                self.controller.available_themes[5])  # Устанавливаем первую доступную тему по умолчанию
-        self.theme_combobox.pack(anchor="se", padx=5, pady=5)  # Упаковываем combobox
+            self.theme_combobox.set(self.controller.available_themes[0])
+        self.theme_combobox.grid(row=4, column=1, padx=65, pady=5, ipady=2, rowspan=4, sticky='w')
 
         self.apply_button = ttk.Button(self, text="Применить", command=self.apply_theme)
-        self.apply_button.pack(anchor="se", padx=5, pady=5)  # Упаковываем кнопку
+        self.apply_button.grid(row=4, column=1, padx=5, pady=5, sticky='e')
+
         self.populate_file_list(os.path.dirname(os.path.abspath(__file__)))
+
+        # Конфигурация grid
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(3, weight=1)
 
     def apply_theme(self):
         selected_theme = self.theme_combobox.get()  # Получаем выбранную тему
@@ -85,40 +87,41 @@ class StartFrame(ttk.Frame):
         directory = self.dir_entry.get()
 
         if not selected_file:
-            print("No file selected.")
+            messagebox.showerror("Ошибка", "Файл не выбран.")
             return
 
         if not directory:
             directory = os.path.dirname(os.path.abspath(__file__))
 
         file_path = os.path.join(directory, selected_file)
-        print(f"Selected file: {file_path}")
 
         self.controller.start_analysis(file_path)
 
 
 class XMLApp:
     def __init__(self, root):
+        self.tab_control = None
+        self.menu = None
         self.root = root
         self.available_themes = self.root.get_themes()
-
+        self.root.set_theme('blue')
         self.root.title("XML Viewer")
-        self.root.geometry("600x400")  # Установите размер окна
-        self.root.resizable(False, False)  # Запретить изменение размера окна
+        self.root.geometry("700x330+100+100")
+        self.root.resizable(False, False)
         self.init_ui()  # Вызов метода init_ui
-
         self.start_frame = StartFrame(self.root, self)  # Обратите внимание, что мы передаем self как controller
         self.start_frame.pack(expand=tk.YES, fill=tk.BOTH)
 
     def init_ui(self):
-
+        # Инициализация пользовательского интерфейса
         self.tab_control = ttk.Notebook(self.root)
         self.menu = tk.Menu(self.root)
         self.root.config(menu=self.menu)
         self.file_menu = tk.Menu(self.menu)
-        # self.menu.add_cascade(label="File", menu=self.file_menu)
+        #self.menu.add_cascade(label="File", menu=self.file_menu)
         self.file_menu.add_command(label="Open", command=self.load_xml)
 
+    # Метод для загрузки и анализа XML файла
     def load_xml(self, root, tab_control):
         namespaces = {
             'tr': 'urn:IEEE-1636.1:2011:01:TestResults'
@@ -129,63 +132,81 @@ class XMLApp:
                 tab_name = child.get('callerName', child.get('name', 'Unknown'))
                 self.add_tab(child, tab_name, tab_control)
 
-    def populate_tree(self, tree, xml_element):
-        # Ваша логика для добавления элементов в дерево.
-        # Например:
-        for elem in xml_element.findall(".//tr:TestGroup", namespaces={"tr": "TestGroup"}):
-            test_name = elem.find(".//CallerName") or elem.find(".//name")
-            status = elem.find(".//tr:Outcome", namespaces={"tr": "Outcome"})
-            value = elem.find(".//tr:Data/c:Collection/c:Item/c:Datum", namespaces={"c": "Datum"})
-            valid_values = self.get_valid_values(elem)
-
-            tree.insert("", "end", text=test_name.text if test_name else "-",
-                        values=(status.text if status else "-",
-                                value.get("value") if value is not None else "-",
-                                valid_values))
-
-    def get_valid_values(self, elem):
-        limits = elem.find(".//tr:TestResult/tr:TestLimits/tr:Limits", namespaces={"tr": "Limits"})
-        if limits is not None:
-            operator = limits.get("operator")
-            ge_limit = limits.find(".//c:Limit[@comparator='GE']", namespaces={"c": "Limit"})
-            le_limit = limits.find(".//c:Limit[@comparator='LE']", namespaces={"c": "Limit"})
-            if operator == "AND" and ge_limit is not None and le_limit is not None:
-                return f"{ge_limit.find('./c:Datum').get('value')} - {le_limit.find('./c:Datum').get('value')}"
-            elif ge_limit is not None:
-                return f"> {ge_limit.find('./c:Datum').get('value')}"
-            elif le_limit is not None:
-                return f"< {le_limit.find('./c:Datum').get('value')}"
-        return "-"
-
+    # Метод для добавления новой вкладки в Notebook
     def add_tab(self, elem, name, tab_control):
         tab = ttk.Frame(tab_control)
         tab_control.add(tab, text=name)
 
         tree = ttk.Treeview(tab, columns=("Value",), show="tree headings")
         tree.column("#0", width=200)
-        tree.column("Value", width=200)
-        tree.heading("Value", text="Value")
+        tree.column("Status", width=200)
+        tree.heading("Status", text="Status")
         tree.pack(fill='both', expand=True)
 
         self.populate_tree(tree, elem)
 
+    # Метод для добавления элементов в дерево
     def populate_tree(self, tree, elem, parent="", level=1):
+        namespaces = {
+            "tr": "urn:IEEE-1636.1:2011:01:TestResults",
+            "c": "urn:IEEE-1671:2010:Common"
+        }
+
         for child in elem:
             name = child.attrib.get('callerName', child.attrib.get('name', 'Unknown'))
-            value = child.text if child.text else "N/A"
 
-            if name == "Unknown":
-                continue
+            # Извлечение значения атрибута 'value' из тега 'tr:Outcome'
+            outcome = child.find('./tr:Outcome', namespaces=namespaces)
+            status = outcome.get('value') if outcome is not None else 'N/A'
 
-            id = tree.insert(parent, tk.END, text=name, values=(value,))
-            self.populate_tree(tree, child, id, level + 1)
+            # Извлечение данных из тега 'tr:Data'
+            data_elems = child.findall('./tr:Data/c:Collection/c:Item/c:Datum', namespaces=namespaces)
+            data_values = [datum.get('value') for datum in data_elems] if data_elems else ['N/A']
+            data_str = ', '.join(data_values)  # Преобразование списка значений в строку, разделенную запятыми
 
+            # Извлечение данных из тега 'tr:TestResult'
+            test_result_elems = child.findall('.//tr:TestResult', namespaces=namespaces)
+            for test_result in test_result_elems:
+                value_elem = test_result.find("./tr:TestData/c:Datum", namespaces=namespaces)
+                value = value_elem.get("value") if value_elem is not None else 'N/A'
+
+                low_limit_elem = test_result.find(
+                    "./tr:TestLimits/tr:Limits/c:LimitPair/c:Limit[@comparator='GE']/c:Datum", namespaces=namespaces)
+                low_limit = low_limit_elem.get("value") if low_limit_elem is not None else ' '
+
+                high_limit_elem = test_result.find(
+                    "./tr:TestLimits/tr:Limits/c:LimitPair/c:Limit[@comparator='LE']/c:Datum", namespaces=namespaces)
+                high_limit = high_limit_elem.get("value") if high_limit_elem is not None else ' '
+
+                valid_values_str = f"{low_limit} < > {high_limit}"
+
+                if name == "Unknown":
+                    continue
+
+                id = tree.insert(parent, tk.END, text=name, values=(
+                status, value, valid_values_str))  # Добавление статуса, значения и допустимых значений в дерево
+                self.populate_tree(tree, child, id, level + 1)
+
+    # Метод для анализа выбранного файла и отображения результатов в новом окне
     def start_analysis(self, file_path):
         result_window = tk.Toplevel(self.root)
         result_window.title("Result")
 
-        tab_control = ScrollableNotebook(result_window)
+        # Создаем Canvas и Scrollbar для горизонтальной прокрутки
+        canvas = tk.Canvas(result_window)
+        scrollbar = ttk.Scrollbar(result_window, orient="horizontal", command=canvas.xview)
+        frame = ttk.Frame(canvas)
+
+        canvas.config(xscrollcommand=scrollbar.set)
+        scrollbar.pack(side="bottom", fill="x")
+        canvas.pack(side="left", fill="both", expand=True)
+        canvas.create_window((0, 0), window=frame, anchor="nw")
+
+        # Создаем Notebook внутри Frame
+        tab_control = ttk.Notebook(frame)
         tab_control.pack(expand=tk.YES, fill=tk.BOTH)
+
+        frame.bind("<Configure>", lambda e: canvas.config(scrollregion=canvas.bbox("all")))
 
         if not file_path:
             print("No file selected.")
@@ -230,25 +251,21 @@ class XMLApp:
         frame = ttk.Frame(tab_control)
         tab_control.add(frame, text=tab_name)
 
-        # Добавляем Treeview в каждую вкладку
-        tree = ttk.Treeview(frame, columns=("test", "status", "value", "valid_values"))
-        tree.pack(expand=YES, fill=BOTH)
+        tree = ttk.Treeview(frame, columns=("status", "value", "valid_values"))
+        tree.pack(expand=tk.YES, fill=tk.BOTH)
 
-        tree.column("#0", width=270, minwidth=270, stretch="NO")
-        tree.column("test", width=150, minwidth=150, stretch="NO")
-        tree.column("status", width=80, minwidth=80, stretch="NO")
-        tree.column("value", width=80, minwidth=80, stretch="NO")
-        tree.column("valid_values", width=120, minwidth=120, stretch="NO")
+        tree.column("#0", width=270, minwidth=270)
+        tree.column("status", width=150, minwidth=150)
+        tree.column("value", width=150, minwidth=150)
+        tree.column("valid_values", width=150, minwidth=150)
 
-        # Добавляем столбцы
         tree.heading("#0", text="Treeview")
-        tree.heading("test", text="Test")
         tree.heading("status", text="Status")
         tree.heading("value", text="Value")
         tree.heading("valid_values", text="Valid Values")
 
-        # Добавляем информацию из XML элемента в Treeview
         self.populate_tree(tree, xml_element)
+
 
     def populate_full_tree(self, tree, elem, parent="", level=1):
         name = elem.tag
@@ -280,6 +297,7 @@ class Switch(tk.Canvas):
         self.tag_bind('oval', '<1>', self.toggle)
         self.tag_bind('rect', '<1>', self.toggle)
 
+    # Метод для переключения состояния Switch
     def toggle(self, event=None):
         self.value = not self.value
         fill_color = 'green' if self.value else 'red'
@@ -305,6 +323,31 @@ class ScrollableNotebook(ttk.Frame):
 
         self.canvas.pack(side="top", fill="both", expand=True)
         self.scrollbar.pack(side="bottom", fill="x")
+        self.canvas.create_window((0, 0), window=self.notebook, anchor="nw")
+
+    # Метод для обновления области прокрутки при изменении размера Notebook
+    def on_configure(self, event):
+        self.canvas.configure(scrollregion=self.canvas.bbox("all"))
+
+    # Метод для добавления новой вкладки в ScrollableNotebook
+    def add(self, tab, text):
+        self.notebook.add(tab, text=text)
+
+    def pack(self, *args, **kwargs):
+        super().pack(*args, **kwargs)
+
+class VerticalScrollableNotebook(ttk.Frame):
+    def __init__(self, container, *args, **kwargs):
+        super().__init__(container, *args, **kwargs)
+        self.canvas = tk.Canvas(self)
+        self.scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        self.notebook = ttk.Notebook(self.canvas)
+
+        self.notebook.bind("<Configure>", self.on_configure)
+        self.canvas.configure(yscrollcommand=self.scrollbar.set)
+
+        self.canvas.pack(side="left", fill="both", expand=True)
+        self.scrollbar.pack(side="right", fill="y")
         self.canvas.create_window((0, 0), window=self.notebook, anchor="nw")
 
     def on_configure(self, event):
